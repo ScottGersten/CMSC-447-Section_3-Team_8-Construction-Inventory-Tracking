1ebecac055964278fc96dbddd41a94d444d6ecb9
ScottGersten <130173316+ScottGersten@users.noreply.github.com>
2026-03-03 13:47:32 -0500

    Created inventory test page

--- construction_inventory_flutter/lib/main.dart
+++ construction_inventory_flutter/lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
+import 'inventory_page.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -37,7 +38,8 @@ class MyApp extends StatelessWidget {
         //colorScheme: .fromSeed(seedColor: Colors.deepPurple),
         colorScheme: .fromSeed(seedColor: Colors.green),
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      //home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const InventoryPage(),
     );
   }
 }
